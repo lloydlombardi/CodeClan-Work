3f978d09f90d1a53f94b2ddbd76d60893cb18395
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-07-22 09:58:06 +0100

    Week 2 work commits

---
+++ Week_02/Day_04/unit_testing/no_project_example/scripts/reading_in_data_script.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+#check what your working directory is
+getwd()
+
+#using tab navigate to the students.csv which is saved in the 'data' folder in the same folder as this .R file
+read_csv("CodeClan Work/CodeClan_Work/Week_02/Day_04/unit_testing/no_project_example/data/students.csv")
