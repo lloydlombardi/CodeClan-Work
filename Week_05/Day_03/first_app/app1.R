a422c14588a8f6387db302edf8d7a01e3fbe6141
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-11 08:50:30 +0100

    week 5 day 3 shiny

---
+++ Week_05/Day_03/first_app/app1.R
@@ -0,0 +1,70 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(bslib)
+
+olympics_medals <- read_csv("data/olympics_overall_medals.csv")
+
+all_seasons <- unique(olympics_medals$season)
+all_teams <- unique(olympics_medals$team)
+
+olympics_medals <- olympics_medals %>% 
+  mutate(medal = factor(medal, c("Gold", "Silver", "Bronze")))
+
+# User Interface
+ui <- fluidPage(
+  
+  # theme = bs_theme(bootswatch = 'flatly'),
+  # theme = 'path_to/my_stylesheet.css',
+  
+  titlePanel(tags$h1("Olympic Medals")),
+  
+  plotOutput('medal_plot'),
+  
+  fluidRow(
+    
+    column(4, offset = 1,
+           
+           radioButtons(inputId = 'season_input',
+                        label = tags$i('Summer or Winter Olympics?'),
+                        choices = all_seasons
+           )
+    ),
+    
+    column(4,
+           selectInput(inputId = 'team_input',
+                       label = 'Which team?',
+                       choices = all_teams
+           )
+    )
+  ),
+  
+
+  # HTML: HyperText Markup Language
+  tags$a('The Olympics website', href = 'https://www.Olympic.org')
+  
+)
+
+server <- function(input, output) {
+  
+  output$medal_plot <- renderPlot({
+    olympics_medals %>% 
+      filter(team == input$team_input,
+             season == input$season_input) %>%
+      ggplot(aes(x = medal, 
+                 y = count, 
+                 fill = medal)) +
+      geom_col()
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
